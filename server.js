0252e671c63e485fd2549b484a52fe002a5402e1
iibsecouncil <drnkrishnareddy@gmail.com>
2026-01-08 13:03:56 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/auth");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("IIBSE Backend Running");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`IIBSE Backend running on port ${PORT}`);
+});
