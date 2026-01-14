562a0399e1ee90b8f382c0a69ff6d65a45acd15d
iibsecouncil <drnkrishnareddy@gmail.com>
2026-01-14 16:57:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
-const express = require("express");
-const cors = require("cors");
-
-const authRoutes = require("./routes/auth");
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.use("/api", authRoutes);
-
-app.get("/", (req, res) => {
-  res.send("IIBSE Backend Running");
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`IIBSE Backend running on port ${PORT}`);
-});
-
+app.use(
+  cors({
+    origin: [
+      "https://www.iibse.in",
+      "https://iibse.in"
+    ],
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
